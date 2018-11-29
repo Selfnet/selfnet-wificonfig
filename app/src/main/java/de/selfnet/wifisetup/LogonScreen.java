24f1c2d0ee27b66508cb2239365eb94bcef15be2
erik <erik@selfnet.de>
2018-11-29 21:56:01 +0100

    Add Erik to License and add License for repo

--- app/src/main/java/de/selfnet/wifisetup/LogonScreen.java
+++ app/src/main/java/de/selfnet/wifisetup/LogonScreen.java
@@ -1,4 +1,5 @@
 /* Copyright 2013 Wilco Baan Hofman <wilco@baanhofman.nl>
+ * Copyright 2018 Erik Zeiske <erik@selfnet.de>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
